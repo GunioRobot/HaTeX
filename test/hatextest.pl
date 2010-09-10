88af3e11b81b21f74610f0062c046c9a4d122d80
Haroperi <haroperitan@gmail.com>
2010-09-10 18:54:09 +0900

    add test module

---
+++ test/hatextest.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use lib qw( ../lib );
+use Text::Hatex;
+
+my $header =
+'\documentclass{jarticle}
+
+\setlength{\oddsidemargin}{-5mm}
+\setlength{\textwidth}{17cm}
+\setlength{\topmargin}{-5mm}
+\setlength{\textheight}{254mm}
+\pagestyle{empty}
+
+\begin{document}
+\title{Ttile}
+\date{\today}
+\author{Authro Name\\
+学籍番号とか\\
+\texttt{s1011425@coins.tsukuba.ac.jp}}
+\maketitle
+';
+
+my $hooter = '\end{document}
+';
+
+
+my $buf = '';
+foreach (<>) {
+	$buf .= $_;
+}
+print $header . Text::Hatex->parse( $buf) . $hooter;
+
+
